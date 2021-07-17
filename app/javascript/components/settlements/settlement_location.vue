5964c31a699037b7ad5f482997bae834c3f8b8f2
Egan Neuhengen <lightningworks@gmail.com>
2021-07-16 23:31:32 -0700

    More location information, not quite done

--- app/javascript/components/settlements/settlement_location.vue
+++ app/javascript/components/settlements/settlement_location.vue
@@ -1,7 +1,21 @@
 <template>
-  <v-card>
+  <v-card @click="toggleExpanded()">
     <v-card-title>{{ locationTitle(settlement_location, location_type) }}</v-card-title>
     <v-card-subtitle v-if="locationNeedsSubtitle(settlement_location)">{{ locationSubtitle(settlement_location) }}</v-card-subtitle>
+    <div v-show="expanded" class="expanded_content px-4 pb-4" @click.stop.prevent>
+      <p>{{ settlement_location["description"] }}</p>
+      <div v-if="settlement_location.hasOwnProperty('owners')">
+      <h4>Owners</h4>
+        <v-expansion-panels >
+          <v-expansion-panel v-for="owner in settlement_location.owners">
+            <v-expansion-panel-header><b>{{ owner.full_name }}</b> ({{ ownerRaceStr(owner) }})</v-expansion-panel-header>
+            <v-expansion-panel-content>
+              <p v-if="ownerDescriptionFamily(owner) !== 'undefined'">{{ ownerDescriptionFamily(owner) }}</p>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </div>
+    </div>
   </v-card>
 </template>
 
@@ -20,40 +34,70 @@ export default {
   },
   data: function () {
     return {
+      expanded: false
     }
   },
+  mixins: [
+    string_helpers
+  ],
   methods: {
     locationTitle: function (location, location_type) {
       if (location.hasOwnProperty('title')) {
-        return location['title']
+        return location.title
       } else if (location.hasOwnProperty('name')) {
-        return location['name']
+        return location.name
       } else if (location.hasOwnProperty('size')) {
-        return location['size']['name']
+        return location.size.name
       } else {
         return location_type
       }
     },
     locationSubtitle: function (location) {
       if (location.hasOwnProperty('quality')) {
-        return `${location['name']} - ${location['quality']['name']} Quality`
+        return `${location.name} - ${location.quality.name} Quality`
       } else if (location.hasOwnProperty('name')) {
-        return location['name']
+        return location.name
       } else if (location.hasOwnProperty('fervency')) {
-        return `${location['alignment']} - Local following is ${location['fervency']['name']}`
+        return `${location.alignment} - Local following is ${location.fervency.name}`
       }
       
     },
     locationNeedsSubtitle: function (location) {
       return (location.hasOwnProperty('name') && location.hasOwnProperty('title')) || location.hasOwnProperty('fervency')
+    },
+    toggleExpanded: function () {
+      this.expanded = !(this.expanded)
+    },
+    ownerDescription: function(owner) {
+      return "description here"
+    },
+    ownerDescriptionName: function (owner) {
+
+    },
+    ownerDescriptionFamily: function (owner) {
+      if (owner.race != owner.family_race) {
+        const ethnicity = owner.hasOwnProperty('family_ethnicity') ? ` (${this.toTitleCase(owner.family_ethnicity)})` : ''
+        return `Raised by a ${this.toTitleCase(owner.family_race)} family${ethnicity}`
+      } else {
+        return undefined
+      }
+    },
+    ownerRaceStr: function(owner) {
+      const ethnicity = owner.hasOwnProperty('ethnicity') ? ` - ${this.toTitleCase(owner.ethnicity)}` : ''
+      return this.toTitleCase(owner.race) + ethnicity
+    },
+    ownerNameRaceStr: function(owner) {
+      const ethnicity = owner.hasOwnProperty('name_ethnicity') ? ` - ${this.toTitleCase(owner.name_ethnicity)}` : ''
+      return this.toTitleCase(owner.name_race) + ethnicity
     }
-  },
-  mixins: [
-    string_helpers
-  ]
+  }
 }
 </script>
 
 <style scoped>
+
+.expanded_content {
+
+}
 
 </style>
